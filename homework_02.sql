40228c1a8e886933be38fb5755024ad45b708105
herrroaa <tarek.mohamed@northwestern.edu>
2018-07-02 00:16:54 -0500

    Add files via upload

--- homework_02.sql
+++ homework_02.sql
@@ -29,6 +29,50 @@ order by first_name, last_name;
 
 -- 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
 
+use sakila;
+select  country_id, country from country
+where country in ('Afghanistan', 'Bangladesh', 'China');
+
+-- 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
+
+ALTER TABLE actor ADD middle_name VARCHAR(100) not null AFTER first_name;
+
+-- * 3b. You realize that some of these actors have tremendously long last names. Change the data type of the `middle_name` column to `blobs`.
+
+ALTER TABLE actor MODIFY middle_name BLOB;
+
+-- * 3c. Now delete the `middle_name` column.
+
+alter table actor drop column middle_name;
+select * from actor;
+
+
+-- * 4a. List the last names of actors, as well as how many actors have that last name.
+-- there are 200 last names
+
+select last_name from actor;
+SELECT COUNT(last_name)
+FROM actor;
+
+
+-- * 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+
+-- 4c. Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`, 
+-- the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+
+UPDATE actor
+SET first_name =  'HARPO'
+WHERE first_name = 'GROUCHO'  AND last_name= 'WILLIAMS'; 
+
+-- * 5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?
+
+-- * 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+ 
+SELECT address_id, first_name, last_name
+FROM 
+INNER JOIN authors ON books.authorId = authors.id;
+
+
 
 
 
